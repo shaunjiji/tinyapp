31490de1184e907c0122a810a574fb125119c1c9
Shaun <shaunjiji123@gmail.com>
2022-09-06 19:03:30 +0000

    Added GET route for form page, and added a POST route to receive form submission

--- express_server.js
+++ express_server.js
@@ -8,6 +8,8 @@ const urlDatabase = {
   "9sm5xK": "http://wwww.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -16,6 +18,17 @@ app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
 })
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
