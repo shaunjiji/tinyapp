ab35d0755d18c55366d255c93fea4fab2c60da07
Shaun <shaunjiji123@gmail.com>
2022-09-06 21:44:31 +0000

    Added a form element for each shortURL which uses a POST method, added delete button and added route to remove URLS

--- express_server.js
+++ express_server.js
@@ -44,6 +44,15 @@ app.post("/urls", (req, res) => {
   res.redirect("/urls/" + newId);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+
+})
+
+
+
 app.get("/u/:id", (req, res) => {
 const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
