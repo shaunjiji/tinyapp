9692c85e98407e0f0b692f8be7cffcd8e4411f59
Shaun <shaunjiji123@gmail.com>
2022-09-09 19:41:34 +0000

    Created helperfunction urlsForUser, addedlogic to only display urls if the user is logged in and only shows urls that belong to the user

--- express_server.js
+++ express_server.js
@@ -10,10 +10,15 @@ app.use(cookieParser())
 
 //DATABASES
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://wwww.google.com"
+  b2xVn2: {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID",
+  },
+  s9m5xK: {
+    longURL: "http://wwww.google.com",
+    userID: "user2RandomID",
+  },
 };
-
 const users = {
   userRandomID: {
     id: "userRandomID",
@@ -37,7 +42,6 @@ function generateRandomString() {
      }
      return result;
   }
-
 function fetchUserByEmail (email) {
 for(const userID in users) {
   if(users[userID].email === email){
@@ -46,15 +50,36 @@ for(const userID in users) {
 } 
 return null;
 }
+
+function urlsForUser (id) {
+  let newURLDatabase = {};
+  for (const key in urlDatabase){
+    if (urlDatabase[key].userID === id){
+      newURLDatabase[key] = urlDatabase[key];
+       }
+      
+      }
+      return newURLDatabase;
+  }
+
+
+
 app.get("/", (req, res) => {
   console.log("test");
   res.send("Hello!");
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase, user: users[req.cookies["user_id"]]};
-  console.log('templateVars',templateVars);
-  res.render('urls_index', templateVars);
+  if (req.cookies["user_id"]){
+  let currentUserData = urlsForUser(req.cookies["user_id"])
+  const templateVars = {urls: currentUserData, user: users[req.cookies["user_id"]]};
+  return res.render('urls_index', templateVars);
+  }
+  else {
+    return res.status(400).send({
+      message: 'Please login/register to view this page.'
+    });
+  }
 })
 
 app.get("/urls/new", (req, res) => {
@@ -83,9 +108,24 @@ res.render("login", templateVars);
 })
 
 app.get("/urls/:id", (req, res) => { //user is taken to page where you can edit longURLs
-  console.log(req.params.id);
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies["user_id"]]};
-  res.render("urls_show", templateVars);
+  if (req.cookies["user_id"]){
+  let currentUserData = urlsForUser(req.cookies["user_id"]);
+  
+  if(currentUserData[req.params.id]) {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, user: users[req.cookies["user_id"]]};
+  return res.render("urls_show", templateVars);
+  }
+  if(!currentUserData[req.params.id]){
+    return res.status(400).send({
+      message: 'You do not have access to this page since you do not own this URL. Sorry! :('
+    });  
+  }
+}
+  else {
+    return res.status(400).send({
+      message: 'Please login/register to view this page.'
+    });
+  }
 });
 
 
@@ -137,10 +177,13 @@ app.post("/logout", (req, res) => {
   res.redirect("/login");
 })
 
-app.post("/urls", (req, res) => {
+app.post("/urls", (req, res) => { 
   if (req.cookies["user_id"]) {
-  let newId = generateRandomString(req.body)
-  urlDatabase[newId] = req.body['longURL'];
+  let newId = generateRandomString(req.body);
+  urlDatabase[newId] = {
+    longURL: req.body['longURL'],
+    userID: ""
+  };
   console.log(urlDatabase);
   return res.redirect("/urls/" + newId);
   }
@@ -153,16 +196,54 @@ app.post("/urls", (req, res) => {
 });
 
 app.post("/urls/:id/delete", (req, res) => { //allows users to delete entries from databasee
-
+  if (req.cookies["user_id"]){
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
+  }
+  else {
+    return res.status(400).send({
+      message: 'Please login to be able to shorten URLs!'
+    });
+  }
 
 })
 
 app.post("/urls/:id", (req, res) => { // allows user to edit longURLs in database
-  urlDatabase[req.params.id] = req.body['longURL'];
-  res.redirect("/urls"); 
-})
+  if (!req.cookies["user_id"]){
+    return res.status(400).send({
+      message: 'Please login to be able to shorten URLs!'
+    });
+  }
+
+  let currentUserData = urlsForUser(req.cookies["user_id"]);
+  if(currentUserData[req.params.id]) {
+  // urlDatabase[req.params.id].longURL = req.body['longURL']; 
+  urlDatabase[req.params.id] = {
+    longURL: req.body.longURL,
+    userID: req.cookies["user_id"]
+  }
+  console.log(urlDatabase);
+  return res.redirect("/urls"); 
+   }
+  
+  //  if(!currentUserData[req.params.id]){
+  //   return res.status(400).send({
+  //     message: 'You do not have access to this page since you do not own this URL. Sorry! :('
+  //   });
+  // }  
+
+  if(!urlDatabase[req.params.id]){
+    return res.status(400).send({
+      message: 'This id does not exist. Sorry!:('
+    });  
+  }
+  if(!currentUserData[req.params.id]){
+    return res.status(400).send({
+      message: 'You do not have access to this page since you do not own this URL. Sorry! :('
+    });
+  }  
+ 
+});
 
 
 
